8eadf53d12ffa6b52a3b77ddc4b31ddf2b932a21
sylvesad <sylvesad@PC-B122-06.iut-valence.fr>
2020-03-06 15:03:45 +0100

    Creation of constant, table and constructor without parameter

--- src/Map.java
+++ src/Map.java
@@ -1,8 +1,25 @@
 /**
- * 
+ * the Car park
  * @author sylvesad
- *	The carpark
+ *	
  */
 public class Map {
+	/**
+	 * 	The default size of map
+	 */
+	private final static int DEFAULT_SIZE=6;
+	/**
+	 * 	The table of vehicle 
+	 */
+	public Vehicle[][] map;
+	/**
+	 *	
+	 */
+	public Map()
+	{
+		this.map= new Vehicle[DEFAULT_SIZE][DEFAULT_SIZE];
+		
+	}
+	
 
 }
