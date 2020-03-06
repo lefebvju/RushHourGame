a031f84ec2d5e1a2246241ee28f4d78e893441b2
sylvesad <sylvesad@PC-B122-06.iut-valence.fr>
2020-03-06 14:34:44 +0100

    More and more of javadoc, contructor with parameter

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,7 +1,7 @@
 
 /**
  * 
- * the  
+ * the vehicle needed in the car park
  *  
  * @author sylvesad
  * 
@@ -11,21 +11,40 @@
 public class Vehicle 
 {
 	/**
-	 *   the default size of a normal car
+	 *  the default size of a truck
+	 */
+	public final static int TRUCK=3;
+	/**
+	 *  the default size of a normal car
 	 */
 	public final static int DEFAULTSIZE=2;
+	/**
+	 *  the default size of bike
+	 */
+	public final static int BIKE=1;
 	
 	/**
-	 * 
+	 *	the size of the car 
 	 */
 	private final int size;
 	
 	/**
-	 * 
+	 *  the constructor without parameter = create an normal car 
 	 */
 	public Vehicle()
 	{
 		this.size=Vehicle.DEFAULTSIZE;
 	}
+	/**
+	 *	the constructor with an size parameter
+	 * @param s = Size of the vehicle that you want
+	 */
+	public Vehicle(int s)
+	{
+		this.size=s;
+	}
+	
+	
+	
 	
 }
