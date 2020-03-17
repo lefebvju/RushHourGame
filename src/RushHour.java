b7f80ec03fb63bffaa0fc04e412c99cea0b05041
lefeb <lefeb@LAPTOP-T3HINJAR>
2020-03-17 10:53:49 +0100

    creation of the constructor without parameter and elaboration of javadoc

--- src/RushHour.java
+++ src/RushHour.java
@@ -1,4 +1,23 @@
 
+/**
+ * @author lefebvre julien
+ */
 public class RushHour {
 
+	/**
+	 * constructor without parameter of the class RushHour
+	 */
+	public RushHour() {
+		
+	}
+	
+	/**
+	 * method that creates a part
+	 */
+	public void play() {
+		
+	}
+
+	
+	
 }
