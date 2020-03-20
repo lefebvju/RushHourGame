a6c86402aecb061c782c1bf9d8650e7983b93c7b
lefeb <lefeb@LAPTOP-T3HINJAR>
2020-03-20 11:00:21 +0100

    creation of new game

--- src/RushHour.java
+++ src/RushHour.java
@@ -18,15 +18,50 @@ public class RushHour {
 	 */
 	public void play() {
 	
+		Scanner saisie =new Scanner(System.in);
 		Vehicle CarPlay=new Vehicle("moi ",0,2,'x');
-		
+		Vehicle carBlue=new Vehicle("car1",4,0,'y');
+		Vehicle carRed=new Vehicle("car2",3,3,'x');
 		Map Park = new Map();
 		Park.addCar(CarPlay);
+		Park.addCar(carBlue);
+	
+		Park.addCar(carRed);
+		
 		System.out.println(Park.toString());
 		
+		while(CarPlay.getX()!=4) {
+			
+			System.out.println("1:advance\n2:back");
+			int ent=saisie.nextInt();
+			if(ent==1) {
+				
+				System.out.println("1:moi\n2:car1\n3:car2");
+				ent=saisie.nextInt();
+				if(ent==1) {
+					Park.advance(CarPlay);
+				}else if (ent==2) {
+					Park.advance(carBlue);
+				}else if (ent==3) {
+					Park.advance(carRed);}
+			}else if(ent==2) {
+				System.out.println("1:moi\n2:car1\n3:car2");
+				ent=saisie.nextInt();
+				if(ent==1) {
+					Park.back(CarPlay);
+				}else if (ent==2) {
+					Park.back(carBlue);
+				}else if (ent==3) {
+					Park.back(carRed);
+			}}
+			System.out.println(Park.toString());
+		}
+		System.out.println("felicitation");
 		
 	}
+	
+}
 
 	
 	
-}
+
