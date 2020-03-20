57e14bfed7ac6ca52ce8040ddafeb789875c683e
lefeb <lefeb@LAPTOP-T3HINJAR>
2020-03-20 18:10:45 +0100

    modification of the RushHour class for use Level class

--- src/RushHour.java
+++ src/RushHour.java
@@ -17,47 +17,10 @@ public class RushHour {
 	 * method that creates a part
 	 */
 	public void play() {
-	
-		Scanner saisie =new Scanner(System.in);
-		Vehicle CarPlay=new Vehicle("moi ",0,2,'x');
-		Vehicle carBlue=new Vehicle("car1",4,0,'y');
-		Vehicle carRed=new Vehicle("car2",3,3,'x');
-		Map Park = new Map();
-		Park.addCar(CarPlay);
-		Park.addCar(carBlue);
-	
-		Park.addCar(carRed);
-		
-		System.out.println(Park.toString());
-		
-		while(CarPlay.getX()!=4) {
-			
-			System.out.println("1:advance\n2:back");
-			int ent=saisie.nextInt();
-			if(ent==1) {
-				
-				System.out.println("1:moi\n2:car1\n3:car2");
-				ent=saisie.nextInt();
-				if(ent==1) {
-					Park.advance(CarPlay);
-				}else if (ent==2) {
-					Park.advance(carBlue);
-				}else if (ent==3) {
-					Park.advance(carRed);}
-			}else if(ent==2) {
-				System.out.println("1:moi\n2:car1\n3:car2");
-				ent=saisie.nextInt();
-				if(ent==1) {
-					Park.back(CarPlay);
-				}else if (ent==2) {
-					Park.back(carBlue);
-				}else if (ent==3) {
-					Park.back(carRed);
-			}}
-			System.out.println(Park.toString());
-		}
-		System.out.println("felicitation");
-		
+		Level Partie1 = new Level();
+		Partie1.playLevel(1);
+		Partie1.playLevel(2);
+		Partie1.playLevel(3);
 	}
 	/**
 	 * this method gives the ways to move on the board
