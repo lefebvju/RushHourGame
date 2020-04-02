6d397561fd898a6b9a18bb421c0aa998bc39246b
lefeb <lefeb@LAPTOP-T3HINJAR>
2020-04-02 18:11:53 +0200

    modification to adapt to the Direction enumeration

--- src/Map.java
+++ src/Map.java
@@ -38,9 +38,9 @@ public class Map {
 		//TODO (done) please, dont "i" or "j"
 		//TODO (done) "s" isn't better ;-)
 		for(int size=0;size<theVehicle.getSize();size++) {
-			if( theVehicle.getDir()=='x') {
+			if( Direction.horizontal.equals(theVehicle.getDir())) {
 				this.map[theVehicle.getPos().getY()][theVehicle.getPos().getX()+size]=theVehicle;
-			}else if(theVehicle.getDir()=='y') {
+			}else if(Direction.vertical.equals(theVehicle.getDir())) {
 				this.map[theVehicle.getPos().getY()+size][theVehicle.getPos().getX()]=theVehicle;
 			}
 		}
@@ -53,9 +53,9 @@ public class Map {
 	 */
 	public void remove(Vehicle car) {
 		for(int i=0;i<car.getSize();i++) {
-			if( car.getDir()=='x') {
+			if(  Direction.horizontal.equals(car.getDir())) {
 				this.map[car.getPos().getY()][car.getPos().getX()+i]=null;
-			}else if(car.getDir()=='y') {
+			}else if( Direction.vertical.equals(car.getDir())) {
 				this.map[car.getPos().getY()+i][car.getPos().getX()]=null;
 			}
 		}
@@ -71,13 +71,13 @@ public class Map {
 	 * @return true if the requested move is possible otherwise returns false
 	 */
 	public boolean movePossible(Vehicle car,int d,int lim,int aut) {
-		if(car.getDir()=='y') {
+		if( Direction.vertical.equals(car.getDir())) {
 			if(car.getPos().getY()!=lim) 
 				if(this.map[car.getPos().getY()+aut][car.getPos().getX()]==null) {
 					return true;
 					
 			}else {return false;}
-		}else if(car.getDir()=='x') {
+		}else if( Direction.horizontal.equals(car.getDir())) {
 			if(car.getPos().getX()!=lim) 
 			if(this.map[car.getPos().getY()][car.getPos().getX()+aut]==null) {	
 				return true;
@@ -93,11 +93,11 @@ public class Map {
 	 * @param aut is the box on the board where the vehicle wants to go.
 	 */
 public void move(Vehicle car,int d,int lim,int aut) {
-	if(car.getDir()=='y') {
+	if( Direction.vertical.equals(car.getDir())) {
 				this.remove(car);
 				car.getPos().setY(car.getPos().getY()+d);
 				this.addCar(car);
-	}else if(car.getDir()=='x') {	
+	}else if( Direction.horizontal.equals(car.getDir())) {	
 			this.remove(car);
 			car.getPos().setX(car.getPos().getX()+d);
 			this.addCar(car);
@@ -108,9 +108,9 @@ public void move(Vehicle car,int d,int lim,int aut) {
  * this forward function use the function depPos and the function move  
  * @param car
  */
-private void advance(Vehicle car) {
+private void advance( Vehicle car) {
 	if(this.movePossible(car, 1, 6-car.getSize(),car.getSize()))
-	this.move(car, 1, 6-car.getSize(),car.getSize());
+		this.move(car, 1, 6-car.getSize(),car.getSize());
 	else System.out.println("impossible d'avancer");
 }
 
@@ -120,7 +120,7 @@ private void advance(Vehicle car) {
  */
 private void back(Vehicle car) {
 	if(this.movePossible(car, -1,0,-1))
-	this.move(car, -1,0,-1);
+		this.move(car, -1,0,-1);
 	else System.out.println("impossible d'avancer");
 }
 
