e9ea6a35d44fa59c53b3d0fb93f9f21bb0637724
lefeb <lefeb@LAPTOP-T3HINJAR>
2020-05-05 15:29:24 +0200

    add atribute and method in class move

--- src/Move.java
+++ src/Move.java
@@ -1,4 +1,51 @@
 
+/**
+ * it's a class of 
+ * @author lefeb
+ *
+ */
 public class Move {
+	
+/**
+ * 
+ */
+private final Direction dir;
+/**
+ * 
+ */
+private final Position pos;
+/**
+ * 
+ */
+private final int nb_Dep;
+
+/**
+ * @param dir
+ * @param pos
+ * @param nb_Dep
+ */
+public Move(Direction dir,Position pos,int nb_Dep) {
+	this.dir=dir;
+	this.pos=pos;
+	this.nb_Dep=nb_Dep;
+}
+/**
+ * @return Direction for move
+ */
+public Direction getDir() {
+	return this.dir;
+}
+/**
+ * @return Position of the Vehicle is positioned
+ */
+public Position getPos() {
+	return this.pos;
+}
+/**
+ * @return number of move
+ */
+public int getNb_Dep() {
+	return this.nb_Dep;
+}
 
 }
