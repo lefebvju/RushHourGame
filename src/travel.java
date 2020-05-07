1811e1733c74bf80d1835b027197ca9af799ed4d
lefeb <lefeb@LAPTOP-T3HINJAR>
2020-05-07 17:08:14 +0200

    creation of enumeration for the direction of travel

---
+++ src/travel.java
@@ -0,0 +1,17 @@
+
+/**
+ * travel is an enumeration for sens of travel
+ * @author lefeb
+ *
+ */
+public enum travel {
+	/**
+	 * it's going forward
+	 */
+	forth,
+	/**
+	 * it's going backward
+	 */
+	back;
+	
+}
