543d473231a06e9aeac669c6d3653df36a5edfa3
"lefebvju" <"lefebvju"@192.168.1.63>
2020-05-15 11:02:31 +0200

    write method AskMove in class player

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Player {
 
@@ -6,7 +7,18 @@ public class Player {
 	}
 
 	public Move AskMove() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Entrez numéro de vehicule:");
+		int num = sc.nextInt();
+		System.out.println("");
+		int y=0;
+		int x=0;
+		Travel dep=Travel.back;
+		Position pos=new Position(x,y);
+		int nb_dep=0;
+		Move move=new Move(dep,pos,nb_dep);
+		
+		return move;
 	}
 }
