7af28e5647a370364ecae38ce1a878b099e10bf1
lefeb <lefeb@192.168.1.100>
2020-07-16 09:27:23 +0200

    modification so that everything is random

--- src/RandomPlayer.java
+++ src/RandomPlayer.java
@@ -6,7 +6,7 @@ public class RandomPlayer implements Player{
 		Position pos = new Position((int) (Math.random() * 6 ),(int) (Math.random() * 6 ));
 		Travel dep = Travel.randomTravel() ;
 		int nb_dep=(int) (Math.random() * 5 )+1;
-		Move move=new Move(dep, pos, 1);
+		Move move=new Move(dep, pos, nb_dep);
 		System.out.println(pos.getX()+","+pos.getY());
 		return move;
 	}
