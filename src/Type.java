5b93b5b92e6422afb59ba6dcb1444393c475e0c9
lefeb <lefeb@LAPTOP-60KHI70C>
2020-08-10 13:32:15 +0200

    add part of url for the picture

--- src/Type.java
+++ src/Type.java
@@ -9,22 +9,24 @@ public enum Type {
 	/**
 	 * truck is a vehicle with size=3
 	 */
-	truck(3),
+	truck(3,"Truck"),
 	/**
 	 * car is a vehicle with size = 2
 	 */
-	car(2);
+	car(2,"Car");
 	
 	/**
 	 * is the size of vehicle type
 	 */
 	private final int size;
 	
+	private final String TypeImg;
 	/**
 	 * @param size of vehicle type
 	 */
-	Type(int size){
+	Type(int size,String typeImg){
 		this.size=size;
+		this.TypeImg=typeImg;
 	}
 
 	/**
@@ -36,5 +38,9 @@ public enum Type {
 	public static Type randomType() {
 	    int pick = new Random().nextInt(Type.values().length);
 	    return Type.values()[pick];
+	}
+
+	public String getTypeImg() {
+		return TypeImg;
 	}
 }
